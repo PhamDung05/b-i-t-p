a04f74a3a9e832783885480195a5f6038ed0b79e
PhamDung05 <92784761+PhamDung05@users.noreply.github.com>
2021-11-03 23:10:47 +0700

    Add files via upload

---
+++ bài 1.cpp
@@ -0,0 +1,38 @@
+// CppBasics-D05-K13.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Hello BT-D05-K13\n";
+
+    cout << "Hello, My name is Dung\n";
+    cout << "I am 18 years old\n";
+    cout << "I am from Hanoi\n";
+
+    string name;
+    int age;
+    
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello my name is " << name << '\n';
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "I am " << age << " years old"; '\n';
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
